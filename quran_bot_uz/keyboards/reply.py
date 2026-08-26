e9e30e4b9e201f047d81446e2b7025fa4b662ec7
vakhidov0151 <140543690+vakhidov0151@users.noreply.github.com>
2026-08-26 13:12:30 +0500

    Update reply.py

--- quran_bot_uz/keyboards/reply.py
+++ quran_bot_uz/keyboards/reply.py
@@ -5,9 +5,8 @@ def get_main_keyboard():
         keyboard=[
             [KeyboardButton(text="📖 Qur'on o'qish va tinglash")],
             [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="✨ Kun oyati")],
-            [
-                KeyboardButton(text="📍 Joylashuvni jo'natish", request_location=True)
-            ]
+            [KeyboardButton(text="📿 Elektron tasbeh")],
+            [KeyboardButton(text="📍 Joylashuvni jo'natish", request_location=True)]
         ],
         resize_keyboard=True,
         input_field_placeholder="Kerakli bo'limni tanlang 👇"
