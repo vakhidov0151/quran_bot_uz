7459fbe144cac35707489a391834b933ea877bc7
vakhidov0151 <140543690+vakhidov0151@users.noreply.github.com>
2026-08-26 11:51:02 +0500

    Update main.py

--- quran_bot_uz/main.py
+++ quran_bot_uz/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import aiosqlite
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import start, search
@@ -7,15 +8,24 @@ from handlers import start, search
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    # Railway kabi zamonaviy serverlar uchun to'g'ridan-to'g'ri ulanish
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # Yozgan komandalarimizni botga ulaymiz
     dp.include_router(start.router)
     dp.include_router(search.router)
 
-    print("🚀 Bot muvaffaqiyatli ishga tushdi!")
+    # YANGI: Bot yonganda bazaga kirib, "users" jadvalini majburiy tekshiradi va yaratadi
+    async with aiosqlite.connect("data/quran.db") as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            latitude REAL,
+            longitude REAL
+        )
+        """)
+        await db.commit()
+
+    print("🚀 Bot muvaffaqiyatli ishga tushdi va baza to'liq tekshirildi!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
