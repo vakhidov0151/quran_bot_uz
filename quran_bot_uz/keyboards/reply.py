398f758a5837c7907815f796f4d20ff7073659e2
vakhidov0151 <140543690+vakhidov0151@users.noreply.github.com>
2026-08-27 18:56:12 +0500

    Update reply.py

--- quran_bot_uz/keyboards/reply.py
+++ quran_bot_uz/keyboards/reply.py
@@ -6,15 +6,15 @@ def get_main_keyboard(script='latin'):
             [KeyboardButton(text="📖 Qur'on o'qish va tinglash")],
             [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="✨ Kun oyati")],
             [KeyboardButton(text="🤲 Duolar"), KeyboardButton(text="📿 Elektron tasbeh")],
-            [KeyboardButton(text="🧭 Qibla"), KeyboardButton(text="🔍 Qidiruv")],
-            [KeyboardButton(text="⚙️ Sozlamalar")]
+            [KeyboardButton(text="🧭 Qibla"), KeyboardButton(text="✨ Asmo ul-Husna")],
+            [KeyboardButton(text="🔍 Qidiruv"), KeyboardButton(text="⚙️ Sozlamalar")]
         ]
     else:
         kb = [
             [KeyboardButton(text="📖 Қуръон ўқиш ва тинглаш")],
             [KeyboardButton(text="🕌 Намоз вақтлари"), KeyboardButton(text="✨ Кун ояти")],
             [KeyboardButton(text="🤲 Дуолар"), KeyboardButton(text="📿 Электрон тасбеҳ")],
-            [KeyboardButton(text="🧭 Қибла"), KeyboardButton(text="🔍 Қидирув")],
-            [KeyboardButton(text="⚙️ Созламалар")]
+            [KeyboardButton(text="🧭 Қибла"), KeyboardButton(text="✨ Асмо ул-Ҳусна")],
+            [KeyboardButton(text="🔍 Қидирув"), KeyboardButton(text="⚙️ Созламалар")]
         ]
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
