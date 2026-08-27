39eb43b1913fcfc88031dbef1d628dfff18fd8a4
vakhidov0151 <140543690+vakhidov0151@users.noreply.github.com>
2026-08-27 10:32:20 +0500

    Update reply.py

--- quran_bot_uz/keyboards/reply.py
+++ quran_bot_uz/keyboards/reply.py
@@ -1,27 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_keyboard(script='latin'):
-    if script == 'cyrillic':
-        return ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text="📖 Қуръон ўқиш ва тинглаш")],
-                [KeyboardButton(text="🕌 Намоз вақтлари"), KeyboardButton(text="✨ Кун ояти")],
-                [KeyboardButton(text="🤲 Дуолар"), KeyboardButton(text="🔍 Қидирув")],
-                [KeyboardButton(text="📿 Электрон тасбеҳ")],
-                [KeyboardButton(text="📍 Жойлашувни жўнатиш", request_location=True)]
-            ],
-            resize_keyboard=True,
-            input_field_placeholder="Керакли бўлимни танланг 👇"
-        )
+    if script == 'latin':
+        kb = [
+            [KeyboardButton(text="📖 Qur'on o'qish va tinglash")],
+            [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="✨ Kun oyati")],
+            [KeyboardButton(text="🤲 Duolar"), KeyboardButton(text="📿 Elektron tasbeh")],
+            [KeyboardButton(text="🔍 Qidiruv")]
+        ]
     else:
-        return ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text="📖 Qur'on o'qish va tinglash")],
-                [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="✨ Kun oyati")],
-                [KeyboardButton(text="🤲 Duolar"), KeyboardButton(text="🔍 Qidiruv")],
-                [KeyboardButton(text="📿 Elektron tasbeh")],
-                [KeyboardButton(text="📍 Joylashuvni jo'natish", request_location=True)]
-            ],
-            resize_keyboard=True,
-            input_field_placeholder="Kerakli bo'limni tanlang 👇"
-        )
+        kb = [
+            [KeyboardButton(text="📖 Қуръон ўқиш ва тинглаш")],
+            [KeyboardButton(text="🕌 Намоз вақтлари"), KeyboardButton(text="✨ Кун ояти")],
+            [KeyboardButton(text="🤲 Дуолар"), KeyboardButton(text="📿 Электрон тасбеҳ")],
+            [KeyboardButton(text="🔍 Қидирув")]
+        ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
