f78ad49eb76c70b6aba28c8eda3e60bc332454d4
vakhidov0151 <140543690+vakhidov0151@users.noreply.github.com>
2026-08-28 11:56:21 +0500

    Update reply.py

--- quran_bot_uz/keyboards/reply.py
+++ quran_bot_uz/keyboards/reply.py
@@ -4,19 +4,19 @@ def get_main_keyboard(script='latin'):
     if script == 'latin':
         kb = [
             [KeyboardButton(text="📖 Qur'on o'qish va tinglash")],
-            [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="✨ Kun oyati")],
-            [KeyboardButton(text="🤲 Duolar"), KeyboardButton(text="📿 Elektron tasbeh")],
-            [KeyboardButton(text="🧭 Qibla"), KeyboardButton(text="✨ Asmo ul-Husna")],
-            [KeyboardButton(text="💰 Zakot kalkulyatori"), KeyboardButton(text="🔍 Qidiruv")],
-            [KeyboardButton(text="⚙️ Sozlamalar")]
+            [KeyboardButton(text="🕌 Namoz vaqtlari"), KeyboardButton(text="🌙 Saharlik / Iftorlik")],
+            [KeyboardButton(text="🤲 Duolar"), KeyboardButton(text="✨ Kun oyati")],
+            [KeyboardButton(text="🧭 Qibla"), KeyboardButton(text="📿 Elektron tasbeh")],
+            [KeyboardButton(text="💰 Zakot kalkulyatori"), KeyboardButton(text="✨ Asmo ul-Husna")],
+            [KeyboardButton(text="🔍 Qidiruv"), KeyboardButton(text="⚙️ Sozlamalar")]
         ]
     else:
         kb = [
             [KeyboardButton(text="📖 Қуръон ўқиш ва тинглаш")],
-            [KeyboardButton(text="🕌 Намоз вақтлари"), KeyboardButton(text="✨ Кун ояти")],
-            [KeyboardButton(text="🤲 Дуолар"), KeyboardButton(text="📿 Электрон тасбеҳ")],
-            [KeyboardButton(text="🧭 Қибла"), KeyboardButton(text="✨ Асмо ул-Ҳусна")],
-            [KeyboardButton(text="💰 Закот калькулятори"), KeyboardButton(text="🔍 Қидирув")],
-            [KeyboardButton(text="⚙️ Созламалар")]
+            [KeyboardButton(text="🕌 Намоз вақтлари"), KeyboardButton(text="🌙 Саҳарлик / Ифторлик")],
+            [KeyboardButton(text="🤲 Дуолар"), KeyboardButton(text="✨ Кун ояти")],
+            [KeyboardButton(text="🧭 Қибла"), KeyboardButton(text="📿 Электрон тасбеҳ")],
+            [KeyboardButton(text="💰 Закот калькулятори"), KeyboardButton(text="✨ Асмо ул-Ҳусна")],
+            [KeyboardButton(text="🔍 Қидирув"), KeyboardButton(text="⚙️ Созламалар")]
         ]
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
